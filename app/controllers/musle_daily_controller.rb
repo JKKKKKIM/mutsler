114fd8206ac8487a2d2713cbc065421bbcfaec2f
kimJK <blue1228a@naver.com>
2017-07-02 04:02:26 +0000

    jk cc

--- app/controllers/musle_daily_controller.rb
+++ app/controllers/musle_daily_controller.rb
@@ -1,2 +1,12 @@
 class MusleDailyController < ApplicationController
 end
+
+
+def upload
+    
+        pic = params[:pic]
+        uploader =HahaUploader.new
+        uploader.stroe!(pic)
+        
+        redirect_to '/musle_daily/daily_write'
+end
